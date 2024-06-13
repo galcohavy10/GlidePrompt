e984ec248e01677612c837157badc4e64fbb4e65
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2024-06-13 23:43:57 +0300

    added steps, fixed bugs and changed to replicate
    fixed bugs,
    added steps movement more logical

--- src/frontend/src/components/Demo/Demo.jsx
+++ src/frontend/src/components/Demo/Demo.jsx
@@ -14,19 +14,29 @@ function Demo() {
   const [currentStep, setCurrentStep] = useState(0);
 
   useEffect(() => {
-
     const allTasks = [
-        "Create packing lists for of trips",
-        "Write persuasive essays on various topics",
-        "Create weekly meal plans for users",
-        "Draft emails for professional scenarios",
-        "Compose stories tailored to 5 year olds.",
-        "Explain complex concepts in simple terms",
-        "Plan itineraries for various travel destinations",
-        "Develop business ideas from scratch",
-        "Create detailed marketing strategies",
-        "Generate educational content in a fun way",
-      ];
+      "Create packing lists for trips",
+      "Write persuasive essays on various topics",
+      "Create weekly meal plans for users",
+      "Draft emails for professional scenarios",
+      "Compose stories tailored to 5-year-olds",
+      "Explain complex concepts in simple terms",
+      "Plan itineraries for various travel destinations",
+      "Develop business ideas from scratch",
+      "Create detailed marketing strategies",
+      "Generate educational content in a fun way",
+      "Identify NSFW content in text",
+      "Draft emails to construction workers about site updates",
+      "Write cold emails to stock brokers to introduce new tools",
+      "Reply to angry customers in a call center scenario",
+      "Identify and filter out spam emails effectively",
+      "Take orders for food delivery using chat",
+      "Automate responses to frequently asked questions",
+      "Create summaries of financial reports for quick review",
+      "Generate promotional content for social media campaigns",
+      "Develop training materials for AI ethics and compliance"
+    ];
+    
       
     setTaskOptions(shuffleArray(allTasks).slice(0, 3));
   }, []);
@@ -48,10 +58,15 @@ function Demo() {
     return array;
   }
 
+  function goToFirstStep() {
+    setSystemPrompt('');
+    setCurrentStep(0);
+
+  }
+
   const handleTaskOptionClick = (task) => {
     setUserTask(task);
     setCurrentStep(1); // Move to the next step
-
   };
 
   const getSystemPrompt = async (task) => {
@@ -60,6 +75,7 @@ function Demo() {
       role: "user",
       content: `The user has inputted this task: [[  ${task} ]]. Your job is to make an ai chatbot to generate a response to this task, so you need to give extremely good instructions. 
       Start your response with " YOU ARE A ___ AI. " and then give the ai instructions on how to generate a response to the task. 
+      You may also use variables if users are likely to provide inputs for this task. Wrap all variables with two brackets like so: {{variableName}}.
       Example: 
       input: Write a poem about the sea.
 
@@ -90,6 +106,7 @@ function Demo() {
 
   const handleTaskSubmit = async (event) => {
     event.preventDefault();
+    setCurrentStep(1); // Move to the next step
     await getSystemPrompt(userTask);
   };
 
@@ -112,14 +129,14 @@ function Demo() {
   
 
   if (systemPrompt) {
-    return <TestResponses initialPrompt={systemPrompt} />;
+    return <TestResponses initialPrompt={systemPrompt} goToFirstStep={goToFirstStep} initialTask={userTask}/>;
   }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#79fcd3] to-[#00df9a]">
           <div className="text-center mt-8 mb-10">
-          <h2 className="text-xl font-semibold text-gray-800">Don't just take our word for it —</h2>
-          <h1 className="text-3xl font-bold text-gray-800">Give it a try, for free!</h1>
+          <h2 className="text-xl font-semibold text-gray-800">No more switching tabs—</h2>
+          <h1 className="text-3xl font-bold text-gray-800">Test all the models, in 1 place</h1>
         </div>
       <DemoStatusBar currentStep={currentStep} />
       <form onSubmit={handleTaskSubmit} className="w-full max-w-2xl p-10 space-y-6 bg-white rounded-lg shadow-xl transform transition-all hover:scale-105">
