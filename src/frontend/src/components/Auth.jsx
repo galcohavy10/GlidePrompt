09926095e2e6aecede9e72e3aaaf7e759f7d0339
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2024-06-16 17:26:02 +0300

    bugs + auth
    no need for logging config on frontend firebase + fixing auth

--- src/frontend/src/components/Auth.jsx
+++ src/frontend/src/components/Auth.jsx
@@ -1,30 +1,55 @@
 // src/components/Auth.jsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 import { auth } from '../firebase';
 
 const Auth = () => {
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+    });
+
     const uiConfig = {
-      signInSuccessUrl: '/', // Redirect to homepage after sign-in
+      signInSuccessUrl: '/',
       signInOptions: [
         {
           provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          signInMethod: firebase.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD
+          requireDisplayName: false,
         },
       ],
+      callbacks: {
+        signInSuccessWithAuthResult: () => false // Avoid redirects after sign-in
+      },
       tosUrl: '/terms-of-service',
-      privacyPolicyUrl: '/privacy-policy'
+      privacyPolicyUrl: '/privacy-policy',
     };
 
     const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
-    ui.start('#firebaseui-auth-container', uiConfig);
-  }, []);
+    if (!user) { // Initialize the FirebaseUI Widget using Firebase auth if there's no user signed in already.
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+
+    return () => {
+      unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+      ui.delete(); // Delete the FirebaseUI instance when the component unmounts.
+    };
+  }, [user]); // Re-run the effect when the user state changes.
 
   return (
-    <div id="firebaseui-auth-container"></div>
+    <div>
+      {user ? (
+        <div>
+          Successfully signed in! Hi, {user.displayName || "there"}!
+        </div>
+      ) : (
+        <div id="firebaseui-auth-container"></div>
+      )}
+    </div>
   );
 };
 
