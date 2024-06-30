26bfd0b322e9d69e221312de8eca9c203ebfff72
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2024-06-30 22:19:20 +0300

    limit to 10 ip requests

--- src/backend/index.js
+++ src/backend/index.js
@@ -60,7 +60,7 @@ app.get('/', (req, res) => {
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 10, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes'
 });
 
