e6ba6cc8e77fd7d8eee55054d0dc4b4b8092027f
galcohavy10 <96891588+galcohavy10@users.noreply.github.com>
2024-07-04 22:01:09 +0300

    added endpoint to profile for sub cancelation

--- src/frontend/src/components/Profile.jsx
+++ src/frontend/src/components/Profile.jsx
@@ -3,10 +3,35 @@ import { signOut, getAuth } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase'; // Ensure this path matches your actual import path
 import CardsMini from './CardsMini.jsx';
+import axios from 'axios';
 
 const Profile = ({ user }) => {
   const [userData, setUserData] = useState({ creditsRemaining: null, paymentPlan: '', fullName: 'User' });
   const auth = getAuth();
+  const [serverResponse, setServerResponse] = useState(''); // Add this line
+
+  const cancelSubscription = async () => {
+    try {
+      const response = await axios.post('/cancelSubscription', { subscriptionId: userData.stripeSubscriptionId, uid: user.uid });
+      if (response.data.success) {
+        setServerResponse('Subscription canceled successfully');
+        //wait 2 seconds and then clear the server response
+        setTimeout(() => {
+          setServerResponse('');
+        }, 2000);
+      }
+    }
+    catch (error) {
+      console.error('Failed to cancel subscription', error);
+      setServerResponse('Failed to cancel subscription, contact support if this persists');
+      //wait 2 seconds and then clear the server response
+      setTimeout(() => {
+        setServerResponse('');
+      }, 2000);
+    }
+  };
+    
+
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -42,6 +67,10 @@ const Profile = ({ user }) => {
       <div className="mb-4">
         <h2 className="text-md font-bold">Your Plan: {displayPlan(userData.paymentPlan)}</h2>
         <h2 className="text-md font-bold">Credits Remaining: {userData.creditsRemaining}</h2>
+        {userData.paymentPlan !== 'freeTrial' && <button onClick={cancelSubscription} className=" text-red-500 font-bold py-2 px-4 rounded-full">
+          Cancel Subscription
+        </button>}
+        {serverResponse && <p className="text-black">{serverResponse}</p>}
       </div>
       {userData.paymentPlan === 'freeTrial' && <CardsMini />} {/* Conditionally display Cards component */}
       <button onClick={handleLogout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
